bf3020c5f608c726b3ec11ff7d29cbce1b098638
seongju lee <gch03915@naver.com>
2022-07-31 17:37:06 +0900

    프로젝트 생성

---
+++ src/main/java/jipdol2/eunstargram/EunstargramApplication.java
@@ -0,0 +1,13 @@
+package jipdol2.eunstargram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EunstargramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EunstargramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jipdol2/eunstargram/EunstargramApplicationTests.java
@@ -0,0 +1,13 @@
+package jipdol2.eunstargram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EunstargramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
