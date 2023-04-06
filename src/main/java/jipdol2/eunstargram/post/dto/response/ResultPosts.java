0e9b1189ae95c0cdcfb062fa62fba90668801404
seongju lee <gch03915@naver.com>
2023-04-06 23:46:01 +0900

    20230406 : ResultPosts class 생성 + test code 수정

---
+++ src/main/java/jipdol2/eunstargram/post/dto/response/ResultPosts.java
@@ -0,0 +1,25 @@
+package jipdol2.eunstargram.post.dto.response;
+
+import lombok.Getter;
+
+/**
+ * 왜 이 class 를 만들었을까?
+ * - response 에 array 를 그대로 넘겨버리면 API 스팩이 변경되었을때 대처하기에
+ *   매우 유연하지 못하다.
+ *   예를 들어서 회원의 이름을 응답해주도록 array 를 반환해주는 API가 있다고 가정해보자.
+ *   여기서 클라이언트가 응답해주는 회원의 count 값도 같이 넘겨달라고 할때 매우 난감해진다.
+ *   그래서 array 형태의 data 는 한번 더 class 를 사용하여 감싸주는게 좋다.
+ *
+ *   count 값도 같이 넘겨달라고 할때 밑에 class 에서 int count 라는 변수만 추가하면 되기때문
+ */
+@Getter
+public class ResultPosts<T>{
+    private T data;
+
+    public ResultPosts(){
+    }
+
+    public ResultPosts(T data) {
+        this.data = data;
+    }
+}
